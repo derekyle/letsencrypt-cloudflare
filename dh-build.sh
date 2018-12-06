f6f8123f1ce11e27c421c1e27c4564c1360e3df1
derekyle <derek@stonebrookdesigns.com>
2018-12-06 12:11:11 -0800

    Delete dh-build.sh

--- dh-build.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/tnwhitwell/letsencrypt.sh-cloudflare/trigger/${DOCKERHUB_TRIGGER_TOKEN}/
