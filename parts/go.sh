ac1b5af12938ba9a77dd7486b0067b1983ce049f
derekyle <derek@stonebrookdesigns.com>
2018-12-06 13:32:00 -0800

    added conditional to check le registration

--- parts/go.sh
+++ parts/go.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
 if [ -z "$CERT_DOMAINS" ]
+then
 echo $CERT_DOMAINS > /letsencrypt/domains.txt
 fi
+
+if [ "$(ls -A /letsencrypt/accounts)" ]; then
+echo "Terms already accepted."
+else
+./dehydrated --register --accept-terms
+fi
+
 cd /letsencrypt
 ./dehydrated -c -g -t dns-01 -k 'hooks/cloudflare/hook.sh'
